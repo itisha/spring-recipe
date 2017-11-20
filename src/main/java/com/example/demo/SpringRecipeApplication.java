23c7f272bb317ea07b34a73e2942defe8f46c1e7
Tychon <tisha.kuprevich@gmail.com>
2017-11-20 21:31:49 +0300

    Init Commit

--- src/main/java/com/example/demo/SpringRecipeApplication.java
+++ src/main/java/com/example/demo/SpringRecipeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRecipeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringRecipeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeApplication.class, args);
+	}
 }
