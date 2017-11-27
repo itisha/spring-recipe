d60bfdcc06f95125c333c4caf94096e9ae5a784b
Tychon <tisha.kuprevich@gmail.com>
2017-11-27 23:05:15 +0300

    Enum

---
+++ src/main/java/org/tisha/domain/Difficulty.java
@@ -0,0 +1,8 @@
+package org.tisha.domain;
+
+/**
+ * Created by t on 27.11.2017.
+ */
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/org/tisha/domain/Recipe.java
+++ src/main/java/org/tisha/domain/Recipe.java
@@ -3,6 +3,8 @@ package org.tisha.domain;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,6 +33,9 @@ public class Recipe {
     //private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -124,5 +129,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
