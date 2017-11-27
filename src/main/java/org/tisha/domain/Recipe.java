88d32c2e0839ef5fbe7a1fa2c25d07602e4bc042
Tychon <tisha.kuprevich@gmail.com>
2017-11-27 22:39:57 +0300

    One to many

--- src/main/java/org/tisha/domain/Recipe.java
+++ src/main/java/org/tisha/domain/Recipe.java
@@ -1,11 +1,13 @@
 package org.tisha.domain;
 
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
@@ -27,6 +29,8 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -112,5 +116,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
