8522b6819240b2de88a2976414460c163134d2be
Tychon <tisha.kuprevich@gmail.com>
2017-12-04 23:02:29 +0300

    Manage bidirectional setters

--- src/main/java/org/tisha/domain/Recipe.java
+++ src/main/java/org/tisha/domain/Recipe.java
@@ -39,17 +39,17 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients = new HashSet<>();
 
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
-
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @ManyToMany
-    @JoinTable(name = "recipe_category_fooz",
+    @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
@@ -131,7 +131,16 @@ public class Recipe {
     }
 
     public void setNotes(Notes notes) {
+        //bidirectional relationship
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        //bidirectional relationship
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
