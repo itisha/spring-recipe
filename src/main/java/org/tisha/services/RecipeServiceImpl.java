274192e2a5ec76340b7046cd1951e4fdf488d90e
tkuprevich <tkuprevich@exadel.com>
2017-12-13 15:49:44 +0300

    Lombok logging, logging levels

--- src/main/java/org/tisha/services/RecipeServiceImpl.java
+++ src/main/java/org/tisha/services/RecipeServiceImpl.java
@@ -1,16 +1,19 @@
 package org.tisha.services;
 
-import java.util.HashSet;
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tisha.domain.Recipe;
 import org.tisha.repositories.RecipeRepository;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by t on 04.12.2017.
  */
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -22,6 +25,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("RecipeServiceImpl lombok annotated logger");
+
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
