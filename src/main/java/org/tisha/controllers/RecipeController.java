5c2de5a21a3e62996a95ee85e99d1a83ca9f6d77
tkuprevich <tkuprevich@exadel.com>
2017-12-14 17:27:28 +0300

    New recipe page

--- src/main/java/org/tisha/controllers/RecipeController.java
+++ src/main/java/org/tisha/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package org.tisha.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.tisha.commands.RecipeCommand;
 import org.tisha.services.RecipeService;
 
 @Controller
@@ -20,5 +23,18 @@ public class RecipeController {
     public String showById(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @RequestMapping(value = "recipe", method = RequestMethod.POST)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
