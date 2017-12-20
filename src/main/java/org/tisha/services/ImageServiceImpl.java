8c744956fb608f2a7cf54cce11697ed0eac4245f
tkuprevich <tkuprevich@exadel.com>
2017-12-20 14:39:30 +0300

    image upload impl

--- src/main/java/org/tisha/services/ImageServiceImpl.java
+++ src/main/java/org/tisha/services/ImageServiceImpl.java
@@ -2,13 +2,46 @@ package org.tisha.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.tisha.domain.Recipe;
+import org.tisha.repositories.RecipeRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+        this.recipeRepository = recipeService;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
     }
 }
